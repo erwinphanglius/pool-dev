7cf9023e51c65cf6c4ba8a03fd40ce2e40e0a20b
erwinphanglius <erwinphanglius10@gmail.com>
2022-02-26 23:27:31 +0700

    init pool dev

---
+++ src/mappings/factory.ts
@@ -0,0 +1,32 @@
+import {
+  PoolCreation
+} from "../../generated/PoolFactory/PoolFactory"
+import { Factory, Pool } from "../../generated/schema"
+import { MetaversepadTemplate } from "../../generated/templates"
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export function handlePoolCreation(event: PoolCreation): void {
+  let factoryEntity = Factory.load(event.address.toHex())
+  let poolEntity = Pool.load(event.params.poolAddress.toHex())
+
+  if (!factoryEntity) {
+    factoryEntity = new Factory(event.address.toHex())
+  }
+
+  if (!poolEntity) {
+    poolEntity = new Pool(event.params.poolAddress.toHex())
+  }
+
+  poolEntity.createdAt = event.params.timestamp
+  poolEntity.startedAt = event.params.saleStartTime
+  poolEntity.endedAt = event.params.saleEndTime
+  poolEntity.participants = event.params.totalParticipants
+  poolEntity.totalRaised = BigInt.fromI32(0)
+
+  factoryEntity.pool.push(poolEntity.id)
+
+  MetaversepadTemplate.create(event.params.poolAddress)
+
+  factoryEntity.save()
+  poolEntity.save()
+}
